cc86d3029216078862a0093a593d96cfe08c54b0
yulianasep <yuliana.sepulveda@alternova.com>
2023-07-04 23:33:37 -0500

    feat: add main components template to search

---
+++ src/components/RandomPokemon.vue
@@ -0,0 +1,29 @@
+<script setup></script>
+
+<template>
+    <section class="pokemon-container">
+        <picture class="pokemon-image">
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="pokemon" />
+        </picture>
+    </section>
+</template>
+
+<style scoped>
+
+.pokemon-container {
+    background-color: var(--medium-background);
+    border-radius: 1.5rem;
+    width: 500px;
+    height: 500px;
+    margin-left: 13rem;
+    position: relative;
+}
+
+.pokemon-image img {
+    position: absolute;
+    width: 25rem;
+    top: 3rem;
+    left: -8rem;
+}
+
+</style>
---
+++ src/components/SearchPokemon.vue
@@ -0,0 +1,51 @@
+<script setup></script>
+
+<template>
+    <section class="search-container">
+        <div class="title">
+            <h2 >WHO IS THAT POKEMON?</h2>    
+        </div>
+        <div class="search">
+            <p>
+                Find your favorite Pokemon! It's as easy as typing in the name or number corresponding to the National Pokédex and you're done.
+            </p>
+            <div class="search-bar">
+                <input type="text" placeholder="Enter the name or id of the Pokemon(1-898)..."  />
+                <span class="input-icon"></span>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+
+.search-container {
+    width: 400px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+}
+
+
+.title h2{
+    font-size: 3rem;
+    font-weight: 600;
+    text-shadow: var(--color-pop) 2px 5px;
+    line-height: 60px;
+}
+
+.search-bar{
+    background-color: var(--medium-background);
+    height: 2rem;
+}
+
+.search-bar input{
+    background: none;
+    width: 100%;
+    height: 100%;
+    background-color: var(--medium-background);
+    border-radius: 10px;
+    border: none;
+}
+
+</style>
