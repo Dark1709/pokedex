4797f618e45cfebebf4af501399b37f24d523535
dark1709 <darly.leon@alternova.com>
2023-07-04 21:22:19 -0500

    feat: The project is initialized

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,43 @@
+<script setup></script>
+
+<template>
+  <header class="header">
+    <picture class="image">
+      <img src="" alt="logo" />
+    </picture>
+
+    <nav class="navbar">
+      <a href="#">Home</a>
+      <a href="#">Pokemons</a>
+      <a href="">GitHub</a>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: var(--background);
+  padding: 1rem 1rem;
+}
+.image {
+  background-color: blue;
+  width: 30vh;
+  height: 7vh;
+}
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+a {
+  color: white;
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin: 0 1rem;
+}
+</style>
